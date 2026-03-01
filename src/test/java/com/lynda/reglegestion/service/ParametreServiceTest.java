202b62b92dffc0eb1f424d58e6065809af9b8d4f
koutouan1211 <lyndakoutouan@gmail.com>
2026-03-01 00:29:13 +0000

    Test avec junit

---
+++ src/test/java/com/lynda/reglegestion/service/ParametreServiceTest.java
@@ -0,0 +1,31 @@
+package com.lynda.reglegestion.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.lynda.reglegestion.dto.ParametreTransaction;
+
+
+
+public class ParametreServiceTest {
+
+	ParametreService Service= new ParametreService();
+	
+	@Test
+	void testPaiement() {
+	//ON CREE LE REQUEST/on saisir les valeur a comparer
+		ParametreTransaction request=new ParametreTransaction();
+		request.setTypeTransaction("demande de paiement");
+		request.setCategorieSender("P");
+		request.setCategorieRecever("");
+		request.setPaymentType("marchand sur site");
+		
+		//on appele la methode
+		String resultat=Service.channelRule(request);
+		
+		//on verifie le resultat
+		assertEquals("donnée invalidé",resultat);
+	}
+	
+}
